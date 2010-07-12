8e1daab5a1481955f49a8b68f05c2243cc5ea8fd
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2010-07-12 11:43:27 +0000

    massive change to conform all modules with 2.4 - add $bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`"; and replace ` with {$bt} in all sql statements

--- tests/example/process-sortable.php
+++ tests/example/process-sortable.php
@@ -1,10 +1,11 @@
 <?php
-/* This is where you would inject your sql into the database 
+/* This is where you would inject your sql into the database
    but we're just going to format it and send it back
 */
-
+$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
 foreach ($_GET['listItem'] as $position => $item) :
-	$sql[] = "UPDATE `table` SET `position` = $position WHERE `id` = $item";
+
+	$sql[] = "UPDATE {$bt}table{$bt} SET {$bt}position{$bt} = $position WHERE {$bt}id{$bt} = $item";
 endforeach;
 
 print_r ($sql);
