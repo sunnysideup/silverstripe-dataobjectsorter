b2cf7d61960a6ed88a9b862daef64987e4d045d3
pett <pett@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-07-18 00:16:55 +0000

    UPGRADE
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/dataobjectsorter/trunk@4903 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -8,25 +8,5 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START dataobjectsorter MODULE ----------------===================
-//MUST SET
 
-//MAY SET
-// You can extend a DataObject OR SiteTree
-//Object::add_extension('SiteTree', 'DataObjectSorterDOD');
-//Object::add_extension('MyDataObject', 'DataObjectSorterDOD');
-//DataObjectOneFieldUpdateController::set_page_size(10)
 //===================---------------- END dataobjectsorter MODULE ----------------===================
-
-Director::addRules(100, array(
-	//sorter
-	'dataobjectsorter/$Action/$ID/$OtherID/$ThirdID/$FourthID' => 'DataObjectSorterController',
-
-	//one field
-	'dataobjectonefieldupdate/$Action/$ID/$OtherID/$ThirdID' => 'DataObjectOneFieldUpdateController',
-
-	//one field for one record
-	'dataobjectonefieldonerecordupdate/$Action/$ID/$OtherID/$ThirdID' => 'DataObjectOneFieldOneRecordUpdateController',
-
-	//one record
-	'dataobjectonerecordupdate/$Action/$ID/$OtherID/$ThirdID' => 'DataObjectOneRecordUpdateController',
-));
