a9ccd592b0c3f0d25f5e1acafa51a0cd31b9619b
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 04:40:13 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/dataobjectsorter/trunk@5585 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -6,7 +6,3 @@
 *
 **/
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START dataobjectsorter MODULE ----------------===================
-
-//===================---------------- END dataobjectsorter MODULE ----------------===================
