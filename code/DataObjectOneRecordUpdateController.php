6fc7cb4fe8e20b0f9a17f7e16f6a88a92309683c
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:51:05 +1300

    Converted to PSR-2

--- code/DataObjectOneRecordUpdateController.php
+++ code/DataObjectOneRecordUpdateController.php
@@ -6,129 +6,135 @@
  *
  **/
 
-class DataObjectOneRecordUpdateController extends Controller{
+class DataObjectOneRecordUpdateController extends Controller
+{
 
-	public static function popup_link($className, $recordID, $linkText = '') {
-		Requirements::javascript("dataobjectsorter/javascript/jquery.simplemodal-1.4.4.js");
-		Requirements::javascript("dataobjectsorter/javascript/dataobjectmodalpopup.js");
-		Requirements::themedCSS("dataobjectmodalpopup", "dataobjectsorter");
-		if(!$linkText) {
-			$linkText = 'click here to edit';
-		}
-		$obj = singleton($className);
-		if($obj->canEdit()) {
-			$link = '/dataobjectonerecordupdate/show/'.$className."/".$recordID;
-			return '
+    public static function popup_link($className, $recordID, $linkText = '')
+    {
+        Requirements::javascript("dataobjectsorter/javascript/jquery.simplemodal-1.4.4.js");
+        Requirements::javascript("dataobjectsorter/javascript/dataobjectmodalpopup.js");
+        Requirements::themedCSS("dataobjectmodalpopup", "dataobjectsorter");
+        if (!$linkText) {
+            $linkText = 'click here to edit';
+        }
+        $obj = singleton($className);
+        if ($obj->canEdit()) {
+            $link = '/dataobjectonerecordupdate/show/'.$className."/".$recordID;
+            return '
 				<a href="'.$link.'" class="modalPopUp" data-width="800" data-height="600" data-rel="window.open(\''.$link.'\', \'sortlistFor'.$className.$recordID.'\',\'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=600,height=600,left = 440,top = 200\'); return false;">'.$linkText.'</a>';
-		}
-	}
+        }
+    }
 
-	private static $allowed_actions = array("onerecordform", "show", "save");
+    private static $allowed_actions = array("onerecordform", "show", "save");
 
-	function init() {
-		// Only administrators can run this method
-		parent::init();
-		if(!Permission::check("CMS_ACCESS_CMSMain")) {
-			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
-		}
-		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
-		//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
-		//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
-		Requirements::javascript("dataobjectsorter/javascript/dataobjectonerecordupdate.js");
-		$url = Director::absoluteURL("dataobjectonerecordupdate/updaterecord/");
-		Requirements::customScript("DataObjectOneRecordUpdateURL = '".$url."'");
-		Requirements::themedCSS("dataobjectonerecordupdate", "dataobjectsorter");
-	}
+    public function init()
+    {
+        // Only administrators can run this method
+        parent::init();
+        if (!Permission::check("CMS_ACCESS_CMSMain")) {
+            Security::permissionFailure($this, _t('Security.PERMFAILURE', ' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
+        }
+        Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+        //Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
+        //Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+        Requirements::javascript("dataobjectsorter/javascript/dataobjectonerecordupdate.js");
+        $url = Director::absoluteURL("dataobjectonerecordupdate/updaterecord/");
+        Requirements::customScript("DataObjectOneRecordUpdateURL = '".$url."'");
+        Requirements::themedCSS("dataobjectonerecordupdate", "dataobjectsorter");
+    }
 
-	function onerecordform() {
-		$table = $this->SecureTableToBeUpdated();
-		$record = $this->SecureRecordToBeUpdated();
-		$obj = $table::get()->byID($record);
-		if(!$obj) {
-			user_error("record could not be found!", E_USER_ERROR);
-		}
-		if(!$obj->canEdit()) {
-			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
-		}
-		$formFields = $this->getFormFields($obj);
-		if(!$formFields) {
-			user_error("Form Fields could not be Found", E_USER_ERROR);
-		}
-		$fields = new FieldList(
-			new HiddenField("Table", "Table", $table),
-			new HiddenField("Record", "Record", $record)
-		);
-		foreach($formFields as $f) {
-			$fields->push($f);
-		}
+    public function onerecordform()
+    {
+        $table = $this->SecureTableToBeUpdated();
+        $record = $this->SecureRecordToBeUpdated();
+        $obj = $table::get()->byID($record);
+        if (!$obj) {
+            user_error("record could not be found!", E_USER_ERROR);
+        }
+        if (!$obj->canEdit()) {
+            Security::permissionFailure($this, _t('Security.PERMFAILURE', ' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
+        }
+        $formFields = $this->getFormFields($obj);
+        if (!$formFields) {
+            user_error("Form Fields could not be Found", E_USER_ERROR);
+        }
+        $fields = new FieldList(
+            new HiddenField("Table", "Table", $table),
+            new HiddenField("Record", "Record", $record)
+        );
+        foreach ($formFields as $f) {
+            $fields->push($f);
+        }
 
-		$form = new Form(
-			$controller = $this,
-			$name = "OneRecordForm",
-			$fields,
-			$actions = new FieldList(new FormAction("save", "save and close"))
-		);
-		$form->loadDataFrom($obj);
-		return $form;
-	}
+        $form = new Form(
+            $controller = $this,
+            $name = "OneRecordForm",
+            $fields,
+            $actions = new FieldList(new FormAction("save", "save and close"))
+        );
+        $form->loadDataFrom($obj);
+        return $form;
+    }
 
-	function save($data, $form) {
-		$table = $this->SecureTableToBeUpdated();
-		$record = $this->SecureRecordToBeUpdated();
-		$obj = $table::get()->byID($record);
-		$form->saveInto($obj);
-		$obj->write();
-		return '
+    public function save($data, $form)
+    {
+        $table = $this->SecureTableToBeUpdated();
+        $record = $this->SecureRecordToBeUpdated();
+        $obj = $table::get()->byID($record);
+        $form->saveInto($obj);
+        $obj->write();
+        return '
 			<p>Your changes have been saved, please <a href="#" onclick="self.close(); return false;">close window</a>.</p>
 			<script type="text/javascript">self.close();</script>';
-	}
+    }
 
-	function show() {
-		return array();
-	}
-
-
-	public function HumanReadableTableName() {
-		return singleton($this->SecureTableToBeUpdated())->plural_name();
-	}
-
-	public function Link($action = null) {
-		$link = "dataobjectonerecordupdate/";
-		if($action) {
-			$link .= "$action/";
-		}
-		return $link;
-	}
-
-	protected function getFormFields($obj) {
-		return $obj->getFrontEndFields();
-	}
+    public function show()
+    {
+        return array();
+    }
 
 
-	protected function SecureTableToBeUpdated() {
-		if(isset($_POST["Table"])) {
-			$table = addslashes($_POST["Table"]);
-		}
-		else {
-			$table = $this->getRequest()->param("ID");
-		}
-		if(class_exists($table)) {
-			return $table;
-		}
-		else {
-			user_error("could not find record: $table", E_USER_ERROR);
-		}
-	}
+    public function HumanReadableTableName()
+    {
+        return singleton($this->SecureTableToBeUpdated())->plural_name();
+    }
 
-	protected function SecureRecordToBeUpdated() {
-		if(isset($_POST["Record"])) {
-			$recordID = $_POST["Record"];
-		}
-		else {
-			$recordID = $this->getRequest()->param("OtherID");
-		}
-		return intval($recordID);
-	}
+    public function Link($action = null)
+    {
+        $link = "dataobjectonerecordupdate/";
+        if ($action) {
+            $link .= "$action/";
+        }
+        return $link;
+    }
+
+    protected function getFormFields($obj)
+    {
+        return $obj->getFrontEndFields();
+    }
 
 
+    protected function SecureTableToBeUpdated()
+    {
+        if (isset($_POST["Table"])) {
+            $table = addslashes($_POST["Table"]);
+        } else {
+            $table = $this->getRequest()->param("ID");
+        }
+        if (class_exists($table)) {
+            return $table;
+        } else {
+            user_error("could not find record: $table", E_USER_ERROR);
+        }
+    }
+
+    protected function SecureRecordToBeUpdated()
+    {
+        if (isset($_POST["Record"])) {
+            $recordID = $_POST["Record"];
+        } else {
+            $recordID = $this->getRequest()->param("OtherID");
+        }
+        return intval($recordID);
+    }
 }
--- code/DataObjectSorterDOD.php
+++ code/DataObjectSorterDOD.php
@@ -7,137 +7,130 @@
  * @package: dataobjectsorter
  **/
 
-class DataObjectSorterDOD extends DataExtension {
+class DataObjectSorterDOD extends DataExtension
+{
 
-	/**
-	 *
-	 * @var string
-	 */ 
-	private static $sort_field = "";
+    /**
+     *
+     * @var string
+     */
+    private static $sort_field = "";
 
-	/**
-	 * standard SS variable
-	 *
-	 */ 
-	private static $db = array(
-		'Sort' => 'Int'
-	);
+    /**
+     * standard SS variable
+     *
+     */
+    private static $db = array(
+        'Sort' => 'Int'
+    );
 
-	/**
-	 * standard SS variable
-	 *
-	 */ 
-	private static $casting = array(
-		'SortTitle' => 'Varchar'
-	);
+    /**
+     * standard SS variable
+     *
+     */
+    private static $casting = array(
+        'SortTitle' => 'Varchar'
+    );
 
-	/**
-	 * action sort
-	 * @return string
-	 */ 
-	function dodataobjectsort() {
-		$i = 0;
-		$extraSet = '';
-		$extraWhere = '';
-		$sortField = $this->SortFieldForDataObjectSorter();
-		$baseDataClass = ClassInfo::baseDataClass($this->owner->ClassName);
-		if($baseDataClass) {
-			if(isset ($_REQUEST["dos"])) {
-				foreach ($_REQUEST['dos'] as $position => $id) {
-					$id = intval($id);
-					$object = $baseDataClass::get()->byID($id);
-					//we add one because position 0 is not good.
-					$position = intval($position)+1;
-					if($object && $object->canEdit()) {
-						if($object->$sortField != $position) {
-							$object->$sortField = $position;
-							//hack for site tree
-							if($object instanceof SiteTree) {
-								$object->writeToStage('Stage');
-								$object->Publish('Stage', 'Live');
-							}
-							else {
-								$object->write();
-							}
-						}
-						else {
-							//do nothing
-						}
-					}
-					else {
-						return _t("DataObjectSorter.NOACCESS", "You do not have access rights to make these changes.");
-					}
-				}
-			}
-			else {
-				return _t("DataObjectSorter.ERROR2", "Error 2");
-			}
-		}
-		else {
-			return _t("DataObjectSorter.ERROR1", "Error 1");
-		}
-		return _t("DataObjectSorter.UPDATEDRECORDS", "Updated record(s)");
-	}
+    /**
+     * action sort
+     * @return string
+     */
+    public function dodataobjectsort()
+    {
+        $i = 0;
+        $extraSet = '';
+        $extraWhere = '';
+        $sortField = $this->SortFieldForDataObjectSorter();
+        $baseDataClass = ClassInfo::baseDataClass($this->owner->ClassName);
+        if ($baseDataClass) {
+            if (isset($_REQUEST["dos"])) {
+                foreach ($_REQUEST['dos'] as $position => $id) {
+                    $id = intval($id);
+                    $object = $baseDataClass::get()->byID($id);
+                    //we add one because position 0 is not good.
+                    $position = intval($position)+1;
+                    if ($object && $object->canEdit()) {
+                        if ($object->$sortField != $position) {
+                            $object->$sortField = $position;
+                            //hack for site tree
+                            if ($object instanceof SiteTree) {
+                                $object->writeToStage('Stage');
+                                $object->Publish('Stage', 'Live');
+                            } else {
+                                $object->write();
+                            }
+                        } else {
+                            //do nothing
+                        }
+                    } else {
+                        return _t("DataObjectSorter.NOACCESS", "You do not have access rights to make these changes.");
+                    }
+                }
+            } else {
+                return _t("DataObjectSorter.ERROR2", "Error 2");
+            }
+        } else {
+            return _t("DataObjectSorter.ERROR1", "Error 1");
+        }
+        return _t("DataObjectSorter.UPDATEDRECORDS", "Updated record(s)");
+    }
 
-	/**
-	 *
-	 * standard SS method
-	 * @param FieldList $fields
-	 * @return FieldList
-	 */ 
-	public function updateCMSFields(FieldList $fields) {
-		$fields->removeFieldFromTab("Root.Main", $this->SortFieldForDataObjectSorter());
-		if(!$this->owner instanceof SiteTree) {
-			$link = $this->dataObjectSorterPopupLink();
-			$fields->addFieldToTab("Root.Sort", new LiteralField("DataObjectSorterPopupLink", "<h2 class='dataObjectSorterDODLink'>".$link."</h2>"));
-		}
-		return $fields;
-	}
+    /**
+     *
+     * standard SS method
+     * @param FieldList $fields
+     * @return FieldList
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->removeFieldFromTab("Root.Main", $this->SortFieldForDataObjectSorter());
+        if (!$this->owner instanceof SiteTree) {
+            $link = $this->dataObjectSorterPopupLink();
+            $fields->addFieldToTab("Root.Sort", new LiteralField("DataObjectSorterPopupLink", "<h2 class='dataObjectSorterDODLink'>".$link."</h2>"));
+        }
+        return $fields;
+    }
 
-	/**
-	 * simplified method
-	 *
-	 * @param string $filterField
-	 * @param string $filterValue
-	 * @param string $alternativeTitle
-	 * 
-	 * @return string HTML
-	 **/
-	function dataObjectSorterPopupLink($filterField = '', $filterValue = '', $alternativeTitle = '') {
-		if($alternativeTitle) {
-			$linkText = $alternativeTitle;
-		}
-		else {
-			$linkText = "Sort ".$this->owner->plural_name();
-		}
-		return DataObjectSorterController::popup_link($this->owner->ClassName, $filterField, $filterValue, $linkText);
-	}
+    /**
+     * simplified method
+     *
+     * @param string $filterField
+     * @param string $filterValue
+     * @param string $alternativeTitle
+     * 
+     * @return string HTML
+     **/
+    public function dataObjectSorterPopupLink($filterField = '', $filterValue = '', $alternativeTitle = '')
+    {
+        if ($alternativeTitle) {
+            $linkText = $alternativeTitle;
+        } else {
+            $linkText = "Sort ".$this->owner->plural_name();
+        }
+        return DataObjectSorterController::popup_link($this->owner->ClassName, $filterField, $filterValue, $linkText);
+    }
 
-	/**
-	 * returns field name for sorting
-	 * 
-	 * @return string 
-	 **/
-	public function SortFieldForDataObjectSorter() {
-		$sortField = Config::inst()->get("DataObjectSorterDOD", "sort_field");
-		$field = "Sort";
-		if($sortField && $this->owner->hasField($sortField)) {
-			$field = $sortField;
-		}
-		elseif($this->owner->hasField("AlternativeSortNumber")) {
-			$field = "AlternativeSortNumber";
-		}
-		elseif($this->owner->hasField("Sort")) {
-			$field = "Sort";
-		}
-		elseif($this->owner->hasField("SortNumber")) {
-			$field = "SortNumber";
-		}
-		else {
-			user_error("No field Sort or AlternativeSortNumber (or $sortField) was found on data object: ".$class, E_USER_WARNING);
-		}
-		return $field;
-	}
+    /**
+     * returns field name for sorting
+     * 
+     * @return string 
+     **/
+    public function SortFieldForDataObjectSorter()
+    {
+        $sortField = Config::inst()->get("DataObjectSorterDOD", "sort_field");
+        $field = "Sort";
+        if ($sortField && $this->owner->hasField($sortField)) {
+            $field = $sortField;
+        } elseif ($this->owner->hasField("AlternativeSortNumber")) {
+            $field = "AlternativeSortNumber";
+        } elseif ($this->owner->hasField("Sort")) {
+            $field = "Sort";
+        } elseif ($this->owner->hasField("SortNumber")) {
+            $field = "SortNumber";
+        } else {
+            user_error("No field Sort or AlternativeSortNumber (or $sortField) was found on data object: ".$class, E_USER_WARNING);
+        }
+        return $field;
+    }
 }
-
-
