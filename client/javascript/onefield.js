446327a6573571e6c0c6cba0fe0e14d5d9314f21
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-04-08 12:30:57 +1200

    API: better JS application

--- client/javascript/onefield.js
+++ client/javascript/onefield.js
@@ -1,237 +1,152 @@
-/**
- *@author nicolaas[at]sunnysideup . co . nz
- *
- **/
-if (
-  document.getElementById('DataObjectOneFieldUpdateUL') !== null &&
-  typeof document.getElementById('DataObjectOneFieldUpdateUL') !== 'undefined'
-) {
-  ;(function ($) {
-    $(document).ready(function () {
-      DataObjectOneFieldUpdate.init()
-    })
-  })(jQuery)
+document.addEventListener('DOMContentLoaded', () => {
+  const ul = document.getElementById('DataObjectOneFieldUpdateUL')
+  if (!ul) return
 
-  var DataObjectOneFieldUpdate = {
-    ulSelector: '#DataObjectOneFieldUpdateUL',
+  const feedback = document.querySelector('.DataObjectOneFieldUpdateFeedback')
+  const fieldNameInput = document.querySelector('#DataObjectOneFieldUpdateFieldName')
+  const tableNameInput = document.querySelector('#DataObjectOneFieldUpdateTableName')
 
-    inputSelector:
-      '#DataObjectOneFieldUpdateUL input.updateField, #DataObjectOneFieldUpdateUL textarea.updateField, #DataObjectOneFieldUpdateUL select.updateField',
+  const tableName = tableNameInput?.value || ''
+  const fieldName = fieldNameInput?.value || ''
+  const loadingText = 'updating data ...'
 
-    feedbackSelector: '.DataObjectOneFieldUpdateFeedback',
+  const dirtyfixes = () => {
+    const el = ul.querySelector('li span .updateField')
+    if (!el) return
 
-    fieldNameSelector: 'input#DataObjectOneFieldUpdateFieldName',
+    const clone = el.cloneNode(true)
+    clone.name = 'ApplyToAll'
+    clone.id = 'ApplyToAll'
 
-    tableNameSelector: 'input#DataObjectOneFieldUpdateTableName',
-
-    loadingText: 'updating data ...',
-
-    tableName: '',
-
-    fieldName: '',
-
-    dirtyfixes: function () {
-      var elementType = jQuery(
-        '#DataObjectOneFieldUpdateUL li span .updateField'
-      )
-        .first()
-        .prop('nodeName')
-        .toLowerCase()
-      if (elementType == 'select') {
-        var selectID = jQuery(
-          '#DataObjectOneFieldUpdateUL li span .updateField'
-        )
-          .first()
-          .attr('id')
-        jQuery('select#' + selectID)
-          .first()
-          .clone()
-          .attr({ name: 'ApplyToAll', id: 'ApplyToAll' })
-          .insertAfter('#ApplyToAllButton')
-      } else {
-        var inputType = jQuery('#DataObjectOneFieldUpdateUL li input')
-          .first()
-          .attr('type')
-          .toLowerCase()
-        var inputValue = jQuery('#DataObjectOneFieldUpdateUL li input')
-          .first()
-          .val()
-        jQuery("<input type='" + inputType + "' />")
-          .attr({ value: inputValue, name: 'ApplyToAll', id: 'ApplyToAll' })
-          .insertAfter('#ApplyToAllButton')
-      }
-    },
-
-    init: function () {
-      this.dirtyfixes()
-      DataObjectOneFieldUpdate.fieldName = jQuery(
-        DataObjectOneFieldUpdate.fieldNameSelector
-      ).val()
-      DataObjectOneFieldUpdate.tableName = jQuery(
-        DataObjectOneFieldUpdate.tableNameSelector
-      ).val()
-      this.setupChangeListener()
-      this.setupFilter()
-      this.setupApplyAll()
-    },
-
-    setupChangeListener: function () {
-      jQuery(DataObjectOneFieldUpdate.inputSelector)
-        .change(function () {
-          var el = this
-          if (jQuery(el).attr('data-ignore-input-change') == 'true') {
-            return
-          }
-          var idAndValue = DataObjectOneFieldUpdate.retrieveDetailsFromInput(el)
-          var id = idAndValue.id
-          var value = idAndValue.value
-          var ids = new Array()
-          ids.push(id)
-          jQuery(el).closest('li.fieldHolder').addClass('runningUpdate')
-          if (parseInt(id)) {
-            if (DataObjectOneFieldUpdate.fieldName) {
-              jQuery(el).closest('li.fieldHolder').addClass('loading')
-              jQuery(DataObjectOneFieldUpdate.feedbackSelector).html(
-                DataObjectOneFieldUpdate.loadingText
-              )
-              DataObjectOneFieldUpdate.updateServer(ids, value, el)
-            } else {
-              jQuery(DataObjectOneFieldUpdate.feedbackSelector).html(
-                'ERROR: could not find field to update'
-              )
-            }
-          } else {
-            jQuery(DataObjectOneFieldUpdate.feedbackSelector).html(
-              'ERROR: could not find record to update'
-            )
-          }
-        })
-        .closest('li.fieldHolder')
-        .addClass('readyForAction')
-    },
-
-    setupFilter: function () {
-      jQuery('#TextMatchFilter').on('input', function (event) {
-        event.preventDefault()
-        var filterValue = jQuery('#TextMatchFilter').val().toLowerCase()
-        jQuery('#DataObjectOneFieldUpdateUL li label').each(function (
-          index,
-          value
-        ) {
-          var match = true
-          var currentLabel = jQuery(this)
-          var labelText = currentLabel.text().toLowerCase()
-          var filterValueArray = filterValue.split(' ')
-          for (var i = 0, len = filterValueArray.length; i < len; i++) {
-            if (labelText.indexOf(filterValueArray[i]) == -1) {
-              match = false
-            }
-          }
-          if (match) {
-            currentLabel.closest('li').show()
-          } else {
-            currentLabel.closest('li').hide()
-          }
-        })
-      })
-    },
-
-    setupApplyAll: function () {
-      jQuery('#ApplyToAllButton').on('click', function (event) {
-        event.preventDefault()
-        var canDo = confirm(
-          'Are you sure you would like to apply the selected value to all visible elements?'
-        )
-        if (canDo == true) {
-          var applyToAllValue = jQuery('#ApplyToAll').val()
-          var elementType = jQuery('#ApplyToAll').prop('nodeName').toLowerCase()
-          var ids = new Array()
-          var inputSelector = ''
-          if (elementType == 'select') {
-            inputSelector = '#DataObjectOneFieldUpdateUL li:visible select'
-            jQuery(inputSelector).each(function (index, el) {
-              var currentInput = jQuery(el)
-              currentInput.val(applyToAllValue)
-              var idAndValue =
-                DataObjectOneFieldUpdate.retrieveDetailsFromInput(currentInput)
-              ids.push(idAndValue.id)
-              // bypass on change
-              currentInput.attr('data-ignore-input-change', 'true')
-              currentInput.change()
-              currentInput.removeAttr('data-ignore-input-change')
-            })
-          } else {
-            elementType = jQuery('#ApplyToAll').attr('type').toLowerCase()
-            if (elementType == 'checkbox') {
-              applyToAllValue = jQuery('#ApplyToAll').is(':checked') ? 1 : 0
-            }
-            inputSelector = '#DataObjectOneFieldUpdateUL li:visible input'
-            jQuery(inputSelector).each(function (index, el) {
-              var currentInput = jQuery(el)
-              currentInput.val(applyToAllValue)
-              if (elementType === 'checkbox') {
-                currentInput.prop('checked', applyToAllValue ? 'checked' : '')
-              }
-              var idAndValue =
-                DataObjectOneFieldUpdate.retrieveDetailsFromInput(currentInput)
-              ids.push(idAndValue.id)
-              // bypass on change
-              currentInput.attr('data-ignore-input-change', 'true')
-              currentInput.change()
-              currentInput.removeAttr('data-ignore-input-change')
-            })
-          }
-          DataObjectOneFieldUpdate.updateServer(
-            ids,
-            applyToAllValue,
-            inputSelector
-          )
-        }
-      })
-    },
-
-    retrieveDetailsFromInput: function (el) {
-      var nameValue = jQuery(el).attr('name')
-      var nameArray = nameValue.split('/')
-      var table = nameArray[0]
-      if (table !== this.tableName) {
-        alert('ERROR --- ' + table + '!==' + this.tableName)
-        return
-      }
-      var id = nameArray[1]
-      var value = jQuery(el).val()
-      if (jQuery(el).attr('type') == 'checkbox') {
-        value = jQuery(el).is(':checked') ? 1 : 0
-      }
-      return {
-        id: id,
-        value: value
-      }
-    },
-
-    updateServer: function (ids, value, elementSelector) {
-      jQuery(DataObjectOneFieldUpdate.feedbackSelector)
-        .html('Running new update')
-        .addClass('loading')
-      var url =
-        DataObjectOneFieldUpdateURL +
-        this.tableName +
-        '/' +
-        this.fieldName +
-        '/?value=' +
-        encodeURIComponent(value) +
-        '&id=' +
-        ids.join()
-      jQuery.get(url, {}, function (data) {
-        jQuery(DataObjectOneFieldUpdate.feedbackSelector)
-          .html(data)
-          .removeClass('loading')
-        jQuery(elementSelector).closest('li.fieldHolder').addClass('updated')
-        jQuery(elementSelector).closest('li.fieldHolder').removeClass('loading')
-        jQuery(elementSelector)
-          .closest('li.fieldHolder')
-          .removeClass('runningUpdate')
-      })
+    if (el.nodeName.toLowerCase() !== 'select') {
+      clone.type = el.type
+      clone.value = el.value
     }
+
+    document.getElementById('ApplyToAllButton')?.after(clone)
   }
-}
+
+  const retrieveDetailsFromInput = el => {
+    const [table, id] = el.name.split('/')
+    if (table !== tableName) {
+      alert(`ERROR --- ${table} !== ${tableName}`)
+      return {}
+    }
+    const value = el.type === 'checkbox' ? (el.checked ? 1 : 0) : el.value
+    return { id, value }
+  }
+
+  const updateServer = (ids, value, elementSelector) => {
+    if (feedback) {
+      feedback.textContent = 'Running new update'
+      feedback.classList.add('loading')
+    }
+
+    const url = `${DataObjectOneFieldUpdateURL}${tableName}/${fieldName}/?value=${encodeURIComponent(value)}&id=${ids.join()}`
+
+    fetch(url)
+      .then(res => res.text())
+      .then(data => {
+        if (feedback) {
+          feedback.innerHTML = data
+          feedback.classList.remove('loading')
+        }
+        const elements = typeof elementSelector === 'string'
+          ? document.querySelectorAll(elementSelector)
+          : [elementSelector]
+        elements.forEach(el => {
+          const holder = el.closest('li.fieldHolder')
+          if (holder) {
+            holder.classList.remove('loading', 'runningUpdate')
+            holder.classList.add('updated')
+          }
+        })
+      })
+  }
+
+  const setupChangeListener = () => {
+    const inputs = ul.querySelectorAll('input.updateField, textarea.updateField, select.updateField')
+    inputs.forEach(el => {
+      el.closest('li.fieldHolder')?.classList.add('readyForAction')
+      el.addEventListener('change', () => {
+        if (el.dataset.ignoreInputChange === 'true') return
+
+        const { id, value } = retrieveDetailsFromInput(el)
+        if (!id) {
+          feedback.textContent = 'ERROR: could not find record to update'
+          return
+        }
+        if (!fieldName) {
+          feedback.textContent = 'ERROR: could not find field to update'
+          return
+        }
+
+        const holder = el.closest('li.fieldHolder')
+        holder?.classList.add('runningUpdate', 'loading')
+        if (feedback) feedback.textContent = loadingText
+
+        updateServer([id], value, el)
+      })
+    })
+  }
+
+  const setupFilter = () => {
+    const filter = document.getElementById('TextMatchFilter')
+    if (!filter) return
+
+    filter.addEventListener('input', () => {
+      const terms = filter.value.toLowerCase().split(' ')
+      ul.querySelectorAll('li label').forEach(label => {
+        const labelText = label.textContent.toLowerCase()
+        const match = terms.every(term => labelText.includes(term))
+        label.closest('li').style.display = match ? '' : 'none'
+      })
+    })
+  }
+
+  const setupApplyAll = () => {
+    const btn = document.getElementById('ApplyToAllButton')
+    if (!btn) return
+
+    btn.addEventListener('click', e => {
+      e.preventDefault()
+      if (!confirm('Are you sure you would like to apply the selected value to all visible elements?')) return
+
+      const applyToAll = document.getElementById('ApplyToAll')
+      if (!applyToAll) return
+
+      const type = applyToAll.nodeName.toLowerCase()
+      let value = applyToAll.value
+      const ids = []
+
+      let selector = ''
+      if (type === 'select') {
+        selector = '#DataObjectOneFieldUpdateUL li:not([style*="display: none"]) select'
+      } else {
+        const inputType = applyToAll.type.toLowerCase()
+        if (inputType === 'checkbox') value = applyToAll.checked ? 1 : 0
+        selector = '#DataObjectOneFieldUpdateUL li:not([style*="display: none"]) input'
+      }
+
+      document.querySelectorAll(selector).forEach(el => {
+        el.value = value
+        if (el.type === 'checkbox') el.checked = !!value
+
+        const { id } = retrieveDetailsFromInput(el)
+        ids.push(id)
+
+        el.dataset.ignoreInputChange = 'true'
+        el.dispatchEvent(new Event('change'))
+        delete el.dataset.ignoreInputChange
+      })
+
+      updateServer(ids, value, selector)
+    })
+  }
+
+  dirtyfixes()
+  setupChangeListener()
+  setupFilter()
+  setupApplyAll()
+})
+
